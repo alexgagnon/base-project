924138d31024370b9071cac5467b5b9980467577
alexgagnon <asgagnon@hotmail.com>
2018-04-11 17:05:42 -0400

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es6: true,
+    node: true,
+    jest: true
+  },
+  extends: ['eslint:recommended'],
+  parser: 'babel-eslint',
+  parserOptions: {
+    sourceType: 'module'
+  },
+  plugins: ['html'],
+  rules: {
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-unused-vars': process.env.NODE_ENV === 'production' ? 'error' : 'warn',
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    indent: [
+      'error',
+      2,
+      {
+        SwitchCase: 1,
+        MemberExpression: 0
+      }
+    ],
+    quotes: ['error', 'single', {allowTemplateLiterals: true}],
+    semi: ['error', 'always'],
+    'space-in-parens': ['error', 'never'],
+    'object-curly-spacing': ['error', 'never']
+  }
+};
