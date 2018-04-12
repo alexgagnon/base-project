dd7206b58a5146e7f7fdf09ae357f614851fbbab
alexgagnon <asgagnon@hotmail.com>
2018-04-12 17:47:10 -0400

    fix(cleanup): replaced test dir with files
    fixed deleting unempty dir issues

--- cleanup.js
+++ cleanup.js
@@ -1,31 +1,20 @@
 const {promisify} = require('util');
 const fs = require('fs');
-const {rmdirP, unlinkP, truncateP} = {
-  rmdirP: promisify(fs.rmdir),
+const {unlinkP, truncateP} = {
   unlinkP: promisify(fs.unlink),
   truncateP: promisify(fs.truncate)
 };
 
-const dirsToDelete = ['test'];
-const filesToDelete = ['cleanup.js'];
+const filesToDelete = [
+  'sample.css',
+  'sample.js',
+  'sample.test.js',
+  'cleanup.js'
+];
+
 const filesToClean = ['README.md'];
 
 let promises = [];
-
-promises = promises.concat(
-  dirsToDelete.map(async dirname => {
-    await Promise.all(
-      fs.readdirSync(dirname).map(filename => {
-        return unlinkP(filename).catch(
-          () => `Could not delete file ${filename} in ${dirname}`
-        );
-      })
-    );
-
-    console.log(fs.readdirSync(dirname));
-    return rmdirP(dirname).catch(() => `Could not delete directory ${dirname}`);
-  })
-);
 
 promises = promises.concat(
   filesToDelete.map(filename => {
